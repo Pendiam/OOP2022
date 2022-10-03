1e998be816db6f68edede13523fe434831c7a138
Pendiam <peter.arnold254@gmail.com>
2022-10-03 13:09:54 +0300

    circle area

--- src/datatypes/Kostant.java
+++ src/datatypes/Kostant.java
@@ -1,10 +1,22 @@
 package datatypes;
 
+import java.util.Scanner;
+
 public class Kostant {
-    public static void main(String[] args) {
-        final float PI = 3.14F;
-        
-        
-        System.out.println(PI);
+    static final float PI = 3.14F;
+
+    static double getArea(float radius) {
+        double area = PI * radius * radius;
+        return area;
     }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the radius: ");
+        float rad = input.nextFloat();
+        System.out.printf("The are of a circle with a radius of %f is %f ", rad, getArea(rad));
+        input.close();
+
+    }
+
 }
