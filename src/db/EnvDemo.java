6a820e86ce10a5dc630bab0589c5850df90a5146
Pendiam <peter.arnold254@gmail.com>
2022-11-06 11:31:41 +0300

    updates

--- src/db/EnvDemo.java
+++ src/db/EnvDemo.java
@@ -1,8 +1,27 @@
 package db;
 
+import java.util.Map;
+import java.util.Properties;
+
 public class EnvDemo {
     public static void main(String[] args) {
-        System.out.println("DB_PASS: " + Env.get("DB_PASS"));
-        
+        System.out.println("-----------System Environment ---------------------");
+
+
+        Map<String, String> OSenv = System.getenv();
+
+        for (String envName : OSenv.keySet()) {
+            System.out.format("%s=%s%n", envName, OSenv.get(envName));
+        }
+
+        System.out.println("-----------JVM Environment ---------------------");
+
+        Properties jvm = System.getProperties();
+        jvm.list(System.out);
+
+
+        String workingDir = System.getProperty("user.dir");
+
+        System.out.println(workingDir);
     }
 }
