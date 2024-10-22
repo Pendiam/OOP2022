ed727ec91417535c8e38560fd897d68a272b7cd1
Peter Arnold <peter.arnold254@gmail.com>
2024-10-22 16:05:42 +0300

    Update SomeClass.java

--- src/inheritance/SomeClass.java
+++ src/inheritance/SomeClass.java
@@ -2,20 +2,25 @@ package inheritance;
 
 public class SomeClass {
     public static void main(String[] args) {
-        // Person p = new Person();
-        // Student s = new Student();
-        // Undergraduate u = new Undergraduate();
+        Person p = new Person();
+        Student s = new Student();
+        Undergraduate u = new Undergraduate();
 
-        // Person robin = new Student();
-        Student green = new Undergraduate("Green", Gender.Female, 20, "S30/PU/40273/20", "Y2S1");
-        // Person pius = new Undergraduate();
+        System.out.println(s instanceof Person);
+        System.out.println(s instanceof Undergraduate);
+        System.out.println(u instanceof Person);
+        System.out.println(p instanceof Student);
 
-        green.writeOutPut();
+        System.out.println("------------Michael Object--------------");
+        Person michael = new Student();
+        michael.writeOutPut();
 
-        // System.out.println(s instanceof Person);
-        // System.out.println(s instanceof Undergraduate);
-        // System.out.println(u instanceof Person);
-        // System.out.println(u instanceof Student);
+        System.out.println("------------Luta Object--------------");
+
+        Undergraduate luta = new Undergraduate("Conchella Luter", Gender.Female, 20, "SB62/PU/41440/23", "Y2S1");
+        luta.writeOutPut();
+
+        
 
     }
 }
